b913c9d387177eaf434fa861a60187f8825668da
Efrem Yohanis <102489707+efremjon@users.noreply.github.com>
2022-04-07 23:39:28 -0700

    part 20 compelt

--- CMS/settings.py
+++ CMS/settings.py
@@ -124,3 +124,21 @@ STATICFILES_DIRS=[
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+# SMTP Configuration
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = 'efremyohanis116@gmail.com'
+EMAIL_HOST_PASSWORD = 'a/ur14623/10'
+
+# EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+# EMAIL_HOST = 'smtp.gmail.com'
+# EMAIL_HOST_USER = os.environ.get('EMAIL_USER')
+# EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_PASS')
+# EMAIL_PORT = 587
+# EMAIL_USE_TLS = True
+# DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
--- account/urls.py
+++ account/urls.py
@@ -1,9 +1,12 @@
-from django.urls import path
+from django.urls import path,include
 from .import views
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth import views as auth_views
+
 urlpatterns = [
 
 # this is authentication apart.
-
+    path('a/',include('django.contrib.auth.urls')),
     path('register/', views.registerPage, name="re"),
 	path('login/', views.loginPage, name="login"),  
 	path('logout/', views.logoutUser, name="logout"),
@@ -19,5 +22,22 @@ urlpatterns = [
     path('create_order/<int:pk>',views.Customer_Order,name='cust_order'),
     path('Update_order/<int:pk>/',views.Update_Order,name='update_order'),
     path('delete_order/<int:pk>/',views.Delete_Order,name='delete_order'),
+
+
+    #################################################
+
+    path('reset_password/',auth_views.PasswordResetView.as_view(template_name="account/password_reset.html"),
+     name="reset_password"),
+
+    path('reset_password_sent/',auth_views.PasswordResetDoneView.as_view(template_name="account/password_reset_sent.html"), 
+        name="password_reset_done"),
+
+    path('reset/<uidb64>/<token>/',auth_views.PasswordResetConfirmView.as_view(template_name="account/password_reset_form.html"), 
+     name="password_reset_confirm"),
+
+    path('reset_password_complete/',auth_views.PasswordResetCompleteView.as_view(template_name="account/password_reset_done.html"), 
+        name="password_reset_complete"),
+
+ #################################################
 
 ]
